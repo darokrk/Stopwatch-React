f04e2b59ac8ac0ca18f96b51999c2c60e92a07eb
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-22 20:35:03 +0100

    Created state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello my friend</h1>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: false,
+      display: "",
+      times: {
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0
+      },
+      results: []
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Stopwatch</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
